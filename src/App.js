e9b406f3ca5b3c9de544ed72c2d5108b185c59ae
dhanushramudri <ramudridhanush@gmail.com>
2024-04-10 21:10:48 +0530

    added images

--- src/App.js
+++ src/App.js
@@ -33,6 +33,46 @@ const App = () => {
       birdReady = true;
     };
     birdImage.src = "./images/bird.png";
+    //  Upper 1 bar
+    var upper1Ready = false;
+    var upper1Image = new Image();
+    upper1Image.onload = function () {
+      upper1Ready = true;
+    };
+    upper1Image.src = "images/upper.png";
+
+    var upper2Ready = false;
+    var upper2Image = new Image();
+    upper2Image.onload = function () {
+      upper2Ready = true;
+    };
+    upper2Image.src = "images/upper.png";
+    var upper3Ready = false;
+    var upper3Image = new Image();
+    upper3Image.onload = function () {
+      upper3Ready = true;
+    };
+    upper3Image.src = "images/upper.png";
+    var lower1Ready = false;
+    var lower1Image = new Image();
+    lower1Image.onload = function () {
+      lower1Ready = true;
+    };
+    lower1Image.src = "images/lower.png";
+
+    var lower2Ready = false;
+    var lower2Image = new Image();
+    lower2Image.onload = function () {
+      lower2Ready = true;
+    };
+    lower2Image.src = "images/lower.png";
+
+    var lower3Ready = false;
+    var lower3Image = new Image();
+    lower3Image.onload = function () {
+      lower3Ready = true;
+    };
+    lower3Image.src = "images/lower.png";
   }, []);
 
   return (
